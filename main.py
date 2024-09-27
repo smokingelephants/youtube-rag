dec86bffa153656778375feee84901abcdaff8cd
smokingelephants <bbose007@gmail.com>
2024-09-27 14:32:46 -0500

    tests

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # This is the YouTube video we're going to use.
 YOUTUBE_VIDEO = "https://www.youtube.com/watch?v=cdiD-9MMpb0"
+
+print("hello world")
+
+from langchain_openai.chat_models import ChatOpenAI
+
+model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model="gpt-3.5-turbo")
+
+print("hellow")
