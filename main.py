dd754167790840a2b3336045e88ab08fa91db7d6
smokingelephants <bbose007@gmail.com>
2024-09-27 20:38:59 -0500

    random

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+print(os.environ.get("OPEN_AI_KEY"))
+
 # This is the YouTube video we're going to use.
 YOUTUBE_VIDEO = "https://www.youtube.com/watch?v=cdiD-9MMpb0"
 
@@ -14,4 +16,15 @@ from langchain_openai.chat_models import ChatOpenAI
 
 model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model="gpt-3.5-turbo")
 
-print("hellow")
+
+
+model.invoke("What MLB team won the World Series during the COVID-19 pandemic?")
+
+
+
+
+
+
+
+
+print("XXX...done...XXX")
